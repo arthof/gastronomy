38a164b221e391f997d635621f348f09961bf113
Artur Hoffman <arthof@wp.pl>
2018-12-08 16:21:16 +0100

    Create Dish controller.

---
+++ src/Controller/DishController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DishController extends AbstractController
+{
+    /**
+     * @Route("/dish/list", name="dish_list")
+     */
+    public function dish_lis(Request $request)
+    {
+        
+        
+        $response = new Response();
+        $response->setContent(json_encode([
+            [
+                'id' => 1,
+                'name' => 'Salmon',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Pizza',
+            ],
+            
+        ]));
+        $response->headers->set('Content-Type', 'application/json');
+        
+        return $response;
+    }
+}
