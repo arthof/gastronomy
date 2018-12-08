569b1750a317926eba6b4a45d9276fa3c06197fd
Artur Hoffman <arthof@wp.pl>
2018-12-08 16:52:40 +0100

    Add controller method to create a dish.

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Dish;
 
 class DishController extends AbstractController
 {
     /**
      * @Route("/dish/list", name="dish_list")
      */
-    public function dish_lis(Request $request)
+    public function dishList(Request $request)
     {
         
         
@@ -32,4 +33,80 @@ class DishController extends AbstractController
         
         return $response;
     }
+    
+    /**
+     * @Route("/dish/create", name="dish_create")
+     */
+    public function dishCreate(Request $request)
+    {
+        $responseContent = array(
+            'error' => 0,
+            'message' => '',
+        );
+        
+        if($request->getContentType()!='json')
+        {
+            $responseContent['error'] = 1;
+            $responseContent['message'] = 'Content type must by JSON.';
+            return $this->prepareJsonResponse($responseContent);            
+        }
+        
+        if($request->getContent()=='')
+        {
+            $responseContent['error'] = 1;
+            $responseContent['message'] = 'Request can\'t be empty.';
+            return $this->prepareJsonResponse($responseContent);    
+        }
+        
+        $data = json_decode($request->getContent());
+        
+        if(!property_exists($data, 'name'))
+        {
+            $responseContent['error'] = 1;
+            $responseContent['message'] = 'Name field not found.';
+            return $this->prepareJsonResponse($responseContent);
+        }
+        
+        $dish = new Dish();
+        $dish->setName($data->name);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($dish);
+        $result = $em->flush();
+        
+        if($dish->getId())
+        {
+            $responseContent['message'] = 'Dish created, it\'s ID: '. $dish->getId();
+        }
+        else
+        {
+            $responseContent['message'] = 'Dish couln\'t be created';
+        }
+        
+        return $this->prepareJsonResponse($responseContent);            
+    }
+    
+    
+    public function prepareJsonResponse($content)
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode($content));
+        return $response;
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
